4947f65e227f1602d70cf9bf66e42a00d1ac41fd
Syed Amaanullah <syedamaanullah@Syeds-MacBook-Pro.local>
2015-01-20 12:32:01 -0600

    Hackwich

--- MobileMapper/ViewController.m
+++ MobileMapper/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
 
-@interface ViewController ()
+@interface ViewController () <MKMapViewDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property CLLocationManager *locationManager;
+@property MKPointAnnotation *mobileMakersAnnotation;
 
 @end
 
@@ -16,12 +20,54 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.mobileMakersAnnotation = [[MKPointAnnotation alloc] init];
+    self.mobileMakersAnnotation.title = @"Mobile Makers";
+    self.mobileMakersAnnotation.subtitle = @"Where we make shit happen!";
+    self.mobileMakersAnnotation.coordinate = CLLocationCoordinate2DMake(41.89373987, -87.63532979);
+    [self.mapView addAnnotation:self.mobileMakersAnnotation];
+
+    CLGeocoder *geocoder = [[CLGeocoder alloc]init];
+    [geocoder geocodeAddressString:@"Grand Canyon" completionHandler:^(NSArray *placemarks, NSError *error) {
+        for (CLPlacemark *place in placemarks) {
+            MKPointAnnotation *annotation = [[MKPointAnnotation alloc]init];
+            annotation.coordinate = place.location.coordinate;
+            [self.mapView addAnnotation:annotation];
+        }
+    }];
+
+    self.locationManager = [[CLLocationManager alloc]init];
+    [self.locationManager requestWhenInUseAuthorization];
+    self.mapView.showsUserLocation = YES;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
+    MKPinAnnotationView *pin = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:nil];
+
+    if (annotation == mapView.userLocation) {
+        return nil;
+    }
+
+    pin.canShowCallout = YES;
+
+    if (annotation == self.mobileMakersAnnotation) {
+        pin.image = [UIImage imageNamed:@"donbora"];
+    }
+    pin.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+    return pin;
+}
+
+-(void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control {
+    CLLocationCoordinate2D centerCoordinate = view.annotation.coordinate;
+
+    MKCoordinateSpan span;
+    span.latitudeDelta = 0.01;
+    span.longitudeDelta = 0.01;
+
+    MKCoordinateRegion region;
+    region.center = centerCoordinate;
+    region.span = span;
+
+    [self.mapView setRegion:region animated:YES];
 }
 
 @end
